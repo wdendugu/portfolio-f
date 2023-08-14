d7725d1f04f0f0c6abc434ea4caf0934ea682655
wdendugu <wdendugu125@gmail.com>
2023-08-14 18:49:42 -0300

    text removed from skills (hover over for...)

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -14,9 +14,6 @@ const Skills = ({skills}: Props) => {
         <h3 className='absolute top-24 uppercase tracking-[20px] dark:text-gray-800 text-gray-500 text-2xl'>
             Skills
         </h3>
-        <h3 className='absolute top-36 uppercase tracking-[3px] dark:text-gray-800 text-gray-500 text-sm'>
-            Hover over a skill for currency profieciency
-        </h3>
         <motion.div className='grid grid-cols-4 md:grid-cols-5 gap-5 pt-36 sm:pt-0'>
         {skills?.map((skill:Skill) => <SkillsItem key={skill._id} skill={skill}/>)}
         </motion.div>
